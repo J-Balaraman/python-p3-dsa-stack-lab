83e179b37c625eeaadacd05fdfb8622efe372d91
jbalaraman <james.balaraman@gmail.com>
2023-12-26 16:35:55 -0700

    Explored different ways to manipulate stacks

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,42 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
+        self.items = items
+        self.limit = limit
         pass
 
     def isEmpty(self):
-        pass
+        if len(self.items) == 0:
+            return True
+        else:
+            return False
 
     def push(self, item):
-        pass
+        self.items.append(item)
 
     def pop(self):
-        pass
+        if len(self.items) == 0:
+            return None
+        else:
+            return self.items.pop()
 
     def peek(self):
         pass
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        if len(self.items) > self.limit:
+            while len(self.items) != self.limit:
+                self.items.pop()
+        return True
 
     def search(self, target):
-        pass
+        num = 1
+        for element in self.items:
+            if element == target:
+                return (len(self.items) - num)
+            else:
+                num += 1
+        return -1
